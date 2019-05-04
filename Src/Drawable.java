54c4ec7c7327dc7acdf5d882fdd1654833451ec2
JKraut <jacobmatthewkraut@hotmail.com>
2019-05-04 15:56:29 +1000

    Drawable Class:
    Added:
    Private variables
    Accessor and mutator functions

--- Src/Drawable.java
+++ Src/Drawable.java
@@ -1,3 +1,32 @@
-public interface Drawable {
+public class Drawable {
+
+    private char lineColour;
+    private float xCoord;
+    private float yCoord;
+
+    public char GetLineColour(){
+        return this.lineColour;
+    }
+
+    public void SetLineColour(char colour){
+        this.lineColour = colour;
+    }
+
+    public float GetXCoord(){
+        return this.xCoord;
+    }
+
+    public void SetXCoord(float xcoord){
+        this.xCoord = xcoord;
+    }
+
+    public float GetYCoord(){
+        return this.yCoord;
+    }
+
+    public void SetYCoord(float ycoord){
+        this.yCoord = ycoord;
+    }
+
 
 }

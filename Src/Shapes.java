4604d44e44b7b9eb39e848441abae62d50bd8c9e
JKraut <jacobmatthewkraut@hotmail.com>
2019-05-04 16:09:36 +1000

    Shapes Class:
    Change implementation to extension.
    Added private variables.
    Added accessor and mutator functions.

--- Src/Shapes.java
+++ Src/Shapes.java
@@ -1,3 +1,13 @@
-public class Shapes implements Drawable {
+public class Shapes extends Drawable {
+
+    private char fillColour;
+
+    private char GetFillColour(){
+        return this.fillColour;
+    }
+
+    private void SetFillColour(char colour){
+        this.fillColour = colour;
+    }
 
 }

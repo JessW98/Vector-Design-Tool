82da4f1a1e3ba47ad2c851c931d0891e5e314d6c
JessW98 <n10194363@qut.edu.au>
2019-05-25 20:42:43 +1000

    Junit CustomPolygonTest

---
+++ Src/CustomPolygonTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomPolygonTest {
+    CustomPolygon testPolygon;
+    @BeforeEach
+    void setUp() {
+        List<double[]> coordinates = new ArrayList<>();
+        testPolygon = new CustomPolygon(coordinates);
+    }
+
+    @Test
+    void setShapePenColour() {
+        testPolygon.setShapePenColour(Color.pink);
+        assertEquals(Color.pink, testPolygon.getShapePenColour());
+    }
+
+    @Test
+    void setShapeFillColour() {
+        testPolygon.setShapeFillColour(Color.pink);
+        assertEquals(Color.pink, testPolygon.getShapeFillColour());
+    }
+
+    @Test
+    void getShapeType() {
+        assertEquals(testPolygon.GetShapeType(), GUI.ShapeType.POLYGON);
+    }
+
+    @Test
+    void getShape() {
+        assertEquals(testPolygon.GetShape(), testPolygon);
+    }
+
+    @Test
+    void getShapePenColour() {
+        testPolygon.setShapePenColour(Color.pink);
+        assertEquals(Color.pink, testPolygon.getShapePenColour());
+    }
+
+    @Test
+    void getShapeFillColour() {
+        testPolygon.setShapeFillColour(Color.pink);
+        assertEquals(Color.pink, testPolygon.getShapeFillColour());
+    }
+}

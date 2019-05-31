471abe184cf7fbcb5638933dd31b4f7f15250e41
WilliamDaley99 <williamdaley99@gmail.com>
2019-05-31 12:54:07 +1000

    Implemented undo, hotkey won't work though

--- Src/GUI.java
+++ Src/GUI.java
@@ -2,6 +2,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
@@ -247,6 +248,14 @@ public class GUI extends JFrame {
             else if(e.getSource() == save){
                 io.SaveImage(drawingCanvas.getShapesDrawn());
             }
+
+            left.getActionMap().put("undo", new AbstractAction("undo") {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int i = 0;
+                }
+            });
+            left.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "undo");
         };
     }
 
